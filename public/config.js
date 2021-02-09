48824358e718839805caaaf2e7bb84feb0a66b8c
dantedxwolf <teacher.martin@gmail.com>
2021-02-09 13:59:17 +0600

    onRequest cloud functions created

---
+++ public/config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBmKFiPZ1DCKAyskKIbhm0PC0P-y8L1i7I",
+    authDomain: "clouddemo-8fc67.firebaseapp.com",
+    projectId: "clouddemo-8fc67",
+    storageBucket: "clouddemo-8fc67.appspot.com",
+    messagingSenderId: "645411949023",
+    appId: "1:645411949023:web:feaf148af30253c4d11c6a",
+    measurementId: "G-E9XP1XJJDC",
+  };
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  const functions = firebase.functions();
